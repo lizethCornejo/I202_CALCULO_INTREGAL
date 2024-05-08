32e9d4e0975880bb73cce05eb76bbf919612ee4f
Lizzete <lizethfloresj80@gmail.com>
2024-05-08 13:33:18 -0600

    ejempo de una funcion, su derivada integrales

--- I202_Calculo_Integral.m
+++ I202_Calculo_Integral.m
@@ -5,3 +5,19 @@
 %%Derivadea e integrales simbolicas
 pkg load symbolic
 syms x
+%%Definir una funcion
+display ('una funcion')
+fx = x * sin (x)
+
+%%derivar
+display ('derivada')
+diff (fx, x)
+
+%%definir una funcion indefinida
+display ('una funcion indefinida')
+ifx = diff (fx, x)
+
+%% definir una funcion definida
+display ('una funcin definida')
+ifx= double (int (fx, x , 0 , 1))
+
